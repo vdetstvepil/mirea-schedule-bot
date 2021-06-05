7ed024beef68fffd61995f55d279448b6bc2d9e6
Anton Kislyakov <antonkis2012@gmail.com>
2021-06-05 13:04:26 +0300

    Adding botListen() to main.

--- project/main.py
+++ project/main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import parameters
 
 
-# Press the green button in the gutter to run the script.
+def botListen():
+    # Авторизация бота по токену.
+    vk_session = vk_api.VkApi(token=parameters.api_key)
+
+    # Вызываем longpoll.
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+
+    # Слушаем longpoll на наличие событий.
+    for event in longpoll.listen():
+        # Пришло новое сообщение.
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            # Выводим сообщение в консоль.
+            print('New message from {}, text = {}'.format(event.user_id, event.text))
+
+            # Отправляем ответное сообщение.
+            vk.messages.send(
+                user_id=event.user_id,
+                random_id=get_random_id(),
+                message='Ваш текст принят.'
+            )
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    botListen()
